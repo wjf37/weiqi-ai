03c46f2e48d52f486e0307f835165daa1c6a3ca8
wjf37 <78282552+wjf37@users.noreply.github.com>
2025-01-18 21:41:14 +0000

    Added to liberties_check and added basic test file

--- board.py
+++ board.py
@@ -1,9 +1,18 @@
 import numpy as np
+from collections import deque
 
 class Board:
     def __init__(self, size=19):
+        #colour 0 = empty colour 1 = black colour 2 = white
         self.size = size
         self.board = np.zeros((size, size), np.uint8)
+
+        self.directions = {
+            1: [0,1],
+            2:[0,-1],
+            3:[1,0],
+            4:[-1,0]
+        }
 
     def check_in_bounds(self, x, y):
         if x in range(self.size) and y in range(self.size):
@@ -26,8 +35,45 @@ class Board:
             return True
         return False
     
-    def liberties_check(self, colour):
+    def liberties_check(self,x,y,colour):
+        #check the 4 directions. if neighbours are the other colour then -1 from liberties. If they are the same then add that stone to the group and search that stone's neighbours for liberties
+        liberties = 0
+        group = {[x,y]}
+        queue = deque(group)
+        opp = self.opposite_colour(colour)
+
+        while queue:
+            cx, cy = queue.popleft()
+            for d in self.directions:
+                dx, dy = self.directions[d]
+                nx, ny = cx + dx, cy + dy
+                ncoords = [nx,ny]
+                if (nx < 0 or ny < 0  or 
+                    nx == self.size or ny == self.size
+                ):
+                    continue
+                
+                if self.board[nx,ny] == opp:
+                    liberties -= 1
+                
+                elif self.board[nx,ny] == colour:
+                    if ncoords not in group:
+                        group.add(ncoords)
+                        queue.append(ncoords)
+
+                else:
+                    liberties += 1
+
+
         return True
+    
+    def opposite_colour(self, colour):
+        if colour == 1:
+            return 2
+        elif colour == 2:
+            return 1
+        else:
+            return 0
     #make groups of connected stones
     #check liberties of stones
     #
---
+++ test.py
@@ -0,0 +1,7 @@
+import unittest
+import board
+
+
+
+if __name__ == '__main__':
+    unittest.main()
