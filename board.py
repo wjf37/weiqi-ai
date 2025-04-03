7a34ac980da9793e8452c121d8f3953a7554478f
wjf37 <78282552+wjf37@users.noreply.github.com>
2025-04-03 18:28:35 +0100

    Fixed liberties check. Added simple error handling. Added unit tests

--- board.py
+++ board.py
@@ -22,6 +22,10 @@ class Board:
     def place_stone(self, x, y, colour):
         #if x not in range(self.size) and y not in range(self.size):
         #    return False
+        if not self.check_in_bounds(x, y):
+            raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
+        if colour not in [1, 2]:
+            raise ValueError(f"Invalid colour: {colour}. Must be 1 (black) or 2 (white).")
         if self.board[x,y] == 0:
             self.board[x,y] = colour
             return True
@@ -30,16 +34,25 @@ class Board:
     def remove_stone(self, x, y):
         #if x not in range(self.size) and y not in range(self.size):
         #    return False
+        if not self.check_in_bounds(x, y):
+            raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
         if self.board[x,y] != 0:
             self.board[x,y] = 0
             return True
         return False
     
-    def liberties_check(self,x,y,colour):
+    def liberties_check(self,x,y):
         #check the 4 directions. if neighbours are the other colour then -1 from liberties. If they are the same then add that stone to the group and search that stone's neighbours for liberties
+        #currently just return liberties, but I'm not sure whether group should also be returned or something.
+        #to handle overlapping liberties - maybe a visited hashmap.
+        if not self.check_in_bounds(x, y):
+            raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
+        
         liberties = 0
-        group = {[x,y]}
+        group = {(x,y): True}
+        visited = {}
         queue = deque(group)
+        colour = self.board[x,y]
         opp = self.opposite_colour(colour)
 
         while queue:
@@ -47,25 +60,26 @@ class Board:
             for d in self.directions:
                 dx, dy = self.directions[d]
                 nx, ny = cx + dx, cy + dy
-                ncoords = [nx,ny]
+                ncoords = (nx,ny)
                 if (nx < 0 or ny < 0  or 
                     nx == self.size or ny == self.size
                 ):
                     continue
-                
-                if self.board[nx,ny] == opp:
-                    liberties -= 1
-                
-                elif self.board[nx,ny] == colour:
+                          
+                if self.board[nx,ny] == colour:
                     if ncoords not in group:
-                        group.add(ncoords)
+                        group[ncoords] = True
                         queue.append(ncoords)
 
+                elif self.board[nx,ny] == opp:
+                    continue
+                
                 else:
-                    liberties += 1
+                    if ncoords not in visited:
+                        liberties += 1
+                        visited[(ncoords)] = True
 
-
-        return True
+        return liberties
     
     def opposite_colour(self, colour):
         if colour == 1:
@@ -80,4 +94,4 @@ class Board:
     #check if in atari
     #check if suicide move
     #check for repeated board state
-    #
+    #figure out a basic gui
