8e58054a376823faeef8c2ed63c057203e18ad7f
wjf37 <78282552+wjf37@users.noreply.github.com>
2025-05-02 19:25:15 +0100

    remaking liberties_check

--- board.py
+++ board.py
@@ -3,10 +3,13 @@ from collections import deque
 
 class Board:
     def __init__(self, size=19):
-        #colour 0 = empty colour 1 = black colour 2 = white
+        #colour 0 = empty colour 1 = black colour -1 = white
         self.size = size
         self.board = np.zeros((size, size), np.uint8)
-
+        #parent stores the parent of each group placed aka the group id
+        self.parent = {}
+        #group_data stores the other stones and liberties left
+        self.group_data = {}
         self.directions = {
             1: [0,1],
             2:[0,-1],
@@ -14,26 +17,39 @@ class Board:
             4:[-1,0]
         }
 
-    def check_in_bounds(self, x, y):
-        if x in range(self.size) and y in range(self.size):
+    def check_in_bounds(self, pos):
+        if pos[0] in range(self.size) and pos[1] in range(self.size):
             return True
         return False
     
-    def place_stone(self, x, y, colour):
-        #if x not in range(self.size) and y not in range(self.size):
-        #    return False
+    def place_stone(self, pos, colour):
+        x, y = pos
         if not self.check_in_bounds(x, y):
             raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
-        if colour not in [1, 2]:
-            raise ValueError(f"Invalid colour: {colour}. Must be 1 (black) or 2 (white).")
-        if self.board[x,y] == 0:
+        if colour not in [1, -1]:
+            raise ValueError(f"Invalid colour: {colour}. Must be 1 (black) or -1 (white).")
+        if self.board[x,y] != 0:
+            raise ValueError(f"Coordinates ({x}, {y}) is already occupied.")
+        else:
             self.board[x,y] = colour
-            return True
-        return False
+            pos = (x,y)
+            self.parent[pos] = pos
+            liberties, enemies, friends = self.liberties_check(pos)
+            
+            if not liberties and not friends:
+                return False
+            
+
+            new_group = {
+                'stones': {pos},
+                'liberties': liberties,
+                'enemy_groups': enemies,
+            }
+            self.group_data[pos] = new_group
     
-    def remove_stone(self, x, y):
-        #if x not in range(self.size) and y not in range(self.size):
-        #    return False
+    #change to remove group
+    def remove_stone(self, pos):
+        x, y = pos
         if not self.check_in_bounds(x, y):
             raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
         if self.board[x,y] != 0:
@@ -41,53 +57,69 @@ class Board:
             return True
         return False
     
-    def liberties_check(self,x,y):
-        #check the 4 directions. if neighbours are the other colour then -1 from liberties. If they are the same then add that stone to the group and search that stone's neighbours for liberties
-        #currently just return liberties, but I'm not sure whether group should also be returned or something.
-        #to handle overlapping liberties - maybe a visited hashmap.
+    def liberties_check(self,pos):
+        #with the changes to the group storage I'll have to change how liberties check works.
+        #It should be cheaper since there won't be repeated calculations.
+        #call liberties check in add stone so that groups can be merged.
+        #union needs to be called in liberties check after all the liberties for the stone have been
+        #calculated.
+        #for remove group: do something like liberties check but for all neighbours so that liberties check
+        #is called for them.
+        x, y = pos
         if not self.check_in_bounds(x, y):
             raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
-        
-        liberties = 0
-        group = {(x,y): True}
-        visited = {}
-        queue = deque(group)
-        colour = self.board[x,y]
-        opp = self.opposite_colour(colour)
-
-        while queue:
-            cx, cy = queue.popleft()
-            for d in self.directions:
-                dx, dy = self.directions[d]
-                nx, ny = cx + dx, cy + dy
-                ncoords = (nx,ny)
-                if (nx < 0 or ny < 0  or 
-                    nx == self.size or ny == self.size
-                ):
-                    continue
-                          
-                if self.board[nx,ny] == colour:
-                    if ncoords not in group:
-                        group[ncoords] = True
-                        queue.append(ncoords)
-
-                elif self.board[nx,ny] == opp:
-                    continue
-                
-                else:
-                    if ncoords not in visited:
-                        liberties += 1
-                        visited[(ncoords)] = True
-
-        return liberties
     
-    def opposite_colour(self, colour):
-        if colour == 1:
-            return 2
-        elif colour == 2:
-            return 1
-        else:
-            return 0
+        colour = self.board[x,y]
+        opp = -colour
+
+        liberties = set()
+        enemies = set()
+        friends = set()
+
+        for d in self.directions:
+            dx, dy = self.directions[d]
+            nx, ny = x + dx, y + dy
+            ncoords = (nx,ny)
+            if (nx < 0 or ny < 0  or 
+                nx == self.size or ny == self.size
+            ):
+                continue
+            #this method gets called when placing a new stone so this new stone should get added to the group of the already existing stone          
+            if self.board[nx,ny] == colour:
+                friends.add(self.find_group((nx,ny)))
+
+            elif self.board[nx,ny] == opp:
+                enemies.add(self.find_group((nx,ny)))
+            
+            else:
+                liberties.add((ncoords))
+        
+        return liberties, enemies, friends
+    
+    def find_group(self,pos):
+        #when merging groups only the parent of the group needs to be updated
+        if self.parent[pos] != pos:
+            self.parent[pos] = self.find_group(self.parent[pos])
+        return self.parent[pos]
+    
+    def union(self, pos1, pos2):
+        #union by rank
+        par1 = self.find_group(pos1)
+        par2 = self.find_group(pos2)
+
+        if par1 == par2:
+            return
+        
+        self.parent[par2] = par1
+        self.group_data[par1]['stones'].update(self.group_data[par2]['stones'])
+        self.group_data[par1]['liberties'].update(self.group_data['liberties'])
+        self.group_data[par1]['liberties'].difference_update(self.group_data[par2]['stones'])
+        del self.group_data[par2]
+        #to calculate the new liberties:
+        #you have two groups that you need to combine as one. This one stone that gets placed need to have two neighbours of the same colour which belong to different groups.
+        #I'm not sure where this check would go, whether the union function just carries out the union or maybe the liberties check handles the neighbours.
+        #I think the new stone would get put into group 1 or somehing, and then union can be carried out and should calculate the correct liberties.
+        
     #make groups of connected stones
     #check liberties of stones
     #
