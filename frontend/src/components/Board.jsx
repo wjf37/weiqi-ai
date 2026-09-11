a6f3fcb422309df1ba4210c0faf7ec8e23110f58
wjf37 <vincentw3007@gmail.com>
2026-09-11 17:52:15 +0100

    added stone grid and click map

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -30,7 +30,7 @@ function getStarPoints(size) {
   return points
 }
 
-function Board({ size = 9 }) {
+function Board({ size = 19 }) {
 // TODO: change cell size to be calculated from a hook checking the window size when a prototype can be shown
     //const windowSize = useWindowSize()
     const cellSize = 40
@@ -79,12 +79,50 @@ function Board({ size = 9 }) {
     )
 }
 
-function Stones() {
+function Stones(board, cellSpacing, offset) {
     //stone grid svg
-
+    //stone size = cellSpacing * 0.9
+    //invisible stone and clickable if board state is 0, else show the stone in its colour according to backend
     //receive updates from backend board state and display stones and their colours as dictated. 
-
     //draw interactable grid --later maybe can also display ghost stone on hover for player who's turn it is
+
+    const clickRadius = cellSpacing * 0.45
+    const stoneRadius = clickRadius * 0.9
+
+    return (
+        <>
+        {board.map((row, y) =>
+            row.map((value, x) => {
+                const cx = offset + x*cellSpacing
+                const cy = offset + y*cellSpacing
+
+                if (value != 0) {
+                    return (
+                        <circle
+                            key={`stone-${x}-${y}`}
+                            cx={cx}
+                            cy={cy}
+                            r={stoneRadius}
+                            fill={value == 1 ? "black":"white"}
+                            //test and see if an outline is needed
+                        />
+                    )
+                }
+
+                return (
+                    <circle
+                        key={`lib-${x}-${y}`}
+                        cx={cx}
+                        cy={cy}
+                        r={clickRadius}
+                        fill="transparent" //thinking about changing to a ghost stone on hover
+                        onClick={() => onPlaceStone(x,y)}
+                        style={{ cursor: "pointer" }}
+                    />
+                )
+            }))}
+        </>
+    )
 }
 
 export { Board, Stones }
